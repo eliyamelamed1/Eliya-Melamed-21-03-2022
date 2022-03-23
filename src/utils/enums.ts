8c2458ad02d419b01055911640fa70c9207d5bd3
eliyamelamed1 <eliyamelamed1@email.com>
2022-03-23 21:34:20 +0200

    changed endpoints to use https

--- src/utils/enums.ts
+++ src/utils/enums.ts
@@ -1,5 +1,5 @@
 export const endpoints = ({ locationKey, apikey, q }: { locationKey?: string; apikey?: string; q?: string }) => ({
-    autoCompleteSearch: `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apikey}&q=${q}&language=en-us`,
-    fiveDaysForecasts: `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${apikey}&language=en-us&details=false&metric=true`,
-    currentConditions: `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apikey}&language=en-us&details=false`,
+    autoCompleteSearch: `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apikey}&q=${q}&language=en-us`,
+    fiveDaysForecasts: `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${apikey}&language=en-us&details=false&metric=true`,
+    currentConditions: `https://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apikey}&language=en-us&details=false`,
 });
