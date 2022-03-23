722c01085418f81f06a6e964b655876905d7e439
eliyamelamed1 <eliyamelamed1@email.com>
2022-03-23 21:00:11 +0200

    added enviorment variable

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import Forecast from '../components/Forecast';
 import React from 'react';
 import SearchBar from '../components/SearchBar';
+import ThemeToggle from '../components/ThemeToggle';
 
 const Home = () => {
     return (
         <div className='home'>
             <SearchBar />
             <Forecast />
+            {/* <ThemeToggle /> */}
         </div>
     );
 };
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,1 @@
-export const apikey = '9gBSe7NmLf0r6tnqb5C4U6KkpqcwLeBy';
+export const apikey = process.env.API_KEY;
