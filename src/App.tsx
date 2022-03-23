23c3bb2bb1f02f813f3e26071667eff4a2a49cc0
eliyamelamed1 <eliyamelamed1@email.com>
2022-03-23 22:31:28 +0200

    added redirection from all other urls to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './styles/main.scss';
 import 'react-toastify/dist/ReactToastify.css';
 
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 import Favorites from './pages/Favorites';
 import Home from './pages/Home';
@@ -20,6 +20,7 @@ function App() {
                 <Routes>
                     <Route path='/' element={<Home />} />
                     <Route path='/favorites' element={<Favorites />} />
+                    <Route path='*' element={<Navigate to='/' />} />
                 </Routes>
             </Router>
         </>
